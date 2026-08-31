fda57d1e03416cf9b97b98c1fd73c07e935ed8c5
adamkosovic <adam.kosovic@icloud.com>
2026-08-31 11:09:33 +0200

    connect to MySQL database

--- index.php
+++ index.php
@@ -1,6 +1,44 @@
 <?php 
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+
+//Connect to our MySQL database. 
+
+$dsn = "mysql:host=localhost;port=3306;dbname=phpdemo;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo -> prepare("SELECT * FROM posts");
+
+$statement -> execute();
+
+$posts = $statement -> fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>" .  $post ['title'] . "</li>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
