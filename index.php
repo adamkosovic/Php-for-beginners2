e745b64d6c544083d33b242ee27398b1cfbf4484
adamkosovic <adam.kosovic@icloud.com>
2026-09-02 19:43:42 +0200

    Handle user input for database queries

--- index.php
+++ index.php
@@ -8,7 +8,10 @@ $config = require "config.php";
 
 $db = new Database($config['database']);
 
-$posts = $db ->query("SELECT * FROM posts") ->fetchAll();
+$id = ($_GET['id']);
+$query = "SELECT * FROM posts WHERE id = ?";
+
+$posts = $db->query($query, [$id])->fetch();
 
 dd($posts);
 
