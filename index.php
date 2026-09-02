a74229c831c05e292b52f2f2226e775c8946e541
adamkosovic <adam.kosovic@icloud.com>
2026-09-02 09:52:21 +0200

    Set up database connection

--- index.php
+++ index.php
@@ -2,27 +2,15 @@
 
 require "functions.php";
 // require "router.php";
+require "Database.php";
 
+$config = require "config.php";
 
-//Connect to our MySQL database. 
+$db = new Database($config['database']);
 
-$dsn = "mysql:host=localhost;port=3306;dbname=phpdemo;user=root;charset=utf8mb4";
+$posts = $db ->query("SELECT * FROM posts") ->fetchAll();
 
-$pdo = new PDO($dsn);
-
-$statement = $pdo -> prepare("SELECT * FROM posts");
-
-$statement -> execute();
-
-$posts = $statement -> fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-  echo "<li>" .  $post ['title'] . "</li>";
-}
-
-
-
-
+dd($posts);
 
 
 
