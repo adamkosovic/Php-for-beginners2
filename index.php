804f7b295f850a09d88f51bbd4c8bee7baaa99ce
adamkosovic <adam.kosovic@icloud.com>
2026-09-04 10:41:18 +0200

    Render the Notes and Note Page

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
 <?php 
 
 require "functions.php";
-// require "router.php";
 require "Database.php";
+require "router.php";
 
-$config = require "config.php";
-
-$db = new Database($config['database']);
-
-$id = ($_GET['id']);
-$query = "SELECT * FROM posts WHERE id = ?";
-
-$posts = $db->query($query, [$id])->fetch();
-
-dd($posts);
 
 
 
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -18,6 +18,11 @@
                     About
                 </a>
 
+                <a href="/notes"
+                  class="<?= urlIs('/notes.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">
+                    Notes
+                </a>
+
                 <a href="/contact"
                   class="<?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">
                     Contact
