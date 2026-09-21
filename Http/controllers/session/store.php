02dc765ed774ada006e486898416f461de81ccc6
adamkosovic <adam.kosovic@icloud.com>
2026-09-21 22:51:36 +0200

    Automatically Redirect Back Upon Failed Validation

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -2,25 +2,18 @@
 
 use Core\Authenticator;
 use Http\Forms\LoginForm;
-use Core\Session;
 
-$email = $_POST['email'];
-$password = $_POST['password']; 
 
-$form = new LoginForm();
-
-if($form->validate($email, $password)){
-  $auth = new Authenticator();
-  
-  if ($auth->attempt($email, $password)){
-    redirect('/');
-  }
-  $form -> error('email', 'No matching account found for that email adress and password.');
-}
-
-Session::flash('errors', $form->errors());
-Session::flash('old', [
-  'email' => $_POST['email']
+$form = LoginForm::validate($attributes = [
+  'email' => $_POST['email'],
+  'password' => $_POST['password']
 ]);
 
-return redirect('/login');
+$signedIn = (new Authenticator)->attempt($attributes['email'], $attributes['password']);
+
+if (! $signedIn) {
+  $form -> error('email', 'No matching account found for that email adress and password.')
+    ->throw();
+}
+
+redirect('/');
